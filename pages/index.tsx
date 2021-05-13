9099c4b8bcc150cecbe6794897c05a3860a8ab07
Marc <marcelopgm65@gmail.com>
2021-05-13 02:37:58 -0300

    Initial context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
-import { GetServerSideProps } from 'next';
-import { FormEvent, useContext, useState } from 'react'
-import { parseCookies } from 'nookies'
+import { FormEvent, useContext, useState } from 'react';
 
 import { AuthContext } from '../contexts/AuthContext';
-import styles from '../styles/Home.module.css'
-import { withSSRGuest } from '../utils/withSSRGuest';
+
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const { signIn } = useContext(AuthContext)
+  const { signIn } = useContext(AuthContext);
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
-
+  
     const data = {
       email,
-      password,
+      password
     }
 
     await signIn(data);
@@ -31,9 +29,3 @@ export default function Home() {
     </form>
   )
 }
-
-export const getServerSideProps = withSSRGuest(async (ctx) => {
-  return {
-    props: {}
-  }
-});
