824877f492c8e52f1e5caeda9ca3e8a976d6f6cf
Marc <marcelopgm65@gmail.com>
2021-11-04 15:57:48 -0300

    :sparkles: feat: Auth Context

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -2,7 +2,7 @@ import { createContext, ReactNode, useEffect, useState } from "react";
 import { setCookie, parseCookies, destroyCookie } from 'nookies';
 import Router from 'next/router';
 
-import { api } from '../services/api';
+import { api } from "../services/apiClient";
 
 type User = {
 	email: string;
@@ -16,7 +16,8 @@ type SignInCredentials = {
 }
 
 type AuthContextData = {
-	signIn(credentials: SignInCredentials): Promise<void>;
+	signIn: (credentials: SignInCredentials) => Promise<void>;
+	signOut: () => void;
 	user: User;
 	isAuthenticated: boolean;
 }
@@ -27,9 +28,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
-export function singOut() {
-	destroyCookie(undefined, 'nextauth.token')
-	destroyCookie(undefined, 'nextauth.refreshToken')
+let authChannel: BroadcastChannel;
+
+export function signOut() {
+	destroyCookie(undefined, 'nextauth.token');
+	destroyCookie(undefined, 'nextauth.refreshToken');
+
+	authChannel.postMessage('signOut');
 
 	Router.push('/');
 }
@@ -37,6 +42,20 @@ export function singOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
 	const [user, setUser] = useState<User>();
 	const isAuthenticated = !!user;
+
+	useEffect(() => {
+		authChannel = new BroadcastChannel('auth');
+
+		authChannel.onmessage = (message) => {
+			switch (message.data) {
+				case 'signOut':
+					signOut();
+					break;
+				default:
+					break;
+			}
+		}
+	}, []);
 
 	useEffect(() => {
 		const { 'nextauth.token': token } = parseCookies();
@@ -48,7 +67,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 				setUser({ email, permissions, roles });
 			})
 			.catch(() => {
-				singOut();
+				signOut();
 			});
 		}
 	}, []);
@@ -87,7 +106,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 	}
 
 	return (
-		<AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+		<AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
 			{children}
 		</AuthContext.Provider>
 	)
